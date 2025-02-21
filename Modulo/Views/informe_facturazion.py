63c040ddfe16518a2f123bdf3eb762035ff2b2f2
JuanTBP300405 <aprendiz.soporte@flagsoluciones.com>
2025-02-21 14:48:28 -0500

    fix informe fac

--- Modulo/Views/informe_facturazion.py
+++ Modulo/Views/informe_facturazion.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 from django.shortcuts import render
-
 from Modulo.forms import FacturacionFilterForm
 from Modulo.models import FacturacionClientes
 
@@ -22,24 +21,26 @@ def filtrar_facturazion(form, factura):
 def obtener_Totales(Meses_info, factura):
     totales = []
     for mes in Meses_info:
-        total_mes = sum(f.Valor or 0 for f in factura if f.Mes == mes)  # Handle None values
+        total_mes = sum(f.Valor or 0 for f in factura if f.Mes == mes)  # Maneja valores None
         totales.append(total_mes)
     return totales
 
-# Función para calcular la información de estudio del empleado
+# Función para calcular la información de facturación
 def obtener_facturazion(factura):
     factura_info = []
-    lineas_info = set()  # Use a set to store unique lines
+    lineas_info = set()  # Usamos un set para evitar duplicados
     Meses_info = set()
 
     for f in factura:   
         datos_factura = {
             'Valor': f.Valor,
+            'Cliente':f.ClienteId
         }
         factura_info.append(datos_factura)
-        lineas_info.add(f.LineaId.Linea)  # Add unique lines to the set
+        lineas_info.add(f.LineaId.Linea)  # Almacenar líneas únicas
         Meses_info.add(f.Mes)
-    return factura_info, list(lineas_info), list(Meses_info)  # Convert sets to lists before returning
+
+    return factura_info, list(lineas_info), list(Meses_info)  # Convertir sets a listas
 
 def facturazion_filtrada(request):
     factura_info = []
@@ -56,6 +57,10 @@ def facturazion_filtrada(request):
             factura_info, lineas_info, Meses_info = obtener_facturazion(factura)
             Total_Meses = obtener_Totales(Meses_info, factura)
             show_data = bool(factura_info)
+
+            # Convertimos en lista de tuplas
+            Meses_info = list(zip(Meses_info, Total_Meses))
+
     else:
         form = FacturacionFilterForm()
 
@@ -63,14 +68,13 @@ def facturazion_filtrada(request):
         'form': form,
         'factura_info': factura_info,
         'lineas_info': lineas_info,
-        'Meses_info': Meses_info,
-        'Total_Meses': Total_Meses,
+        'Meses_info': Meses_info,  # Ahora es una lista de tuplas [(mes, total_mes)]
         'show_data': show_data,
         'mensaje': "No se encontraron resultados para los filtros aplicados." if not factura_info else ""
     }
-    print("esto es lo que trae Meses_info",Meses_info)
-    print("esto es lo que trae Total_Meses",Total_Meses)
-    
+
+    print("este es el cliente",factura_info)
+
+    print("esto es lo que trae Meses_info:", Meses_info)  # Ahora verás [(1, 2.0)]
 
     return render(request, 'informes/informes_facturacion_index.html', context)
-
