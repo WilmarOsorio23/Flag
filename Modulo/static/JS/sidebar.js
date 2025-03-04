6285ddaad672f28d1860806d8a6f6f5bd87bb6af
JuanTBP300405 <aprendiz.soporte@flagsoluciones.com>
2025-03-04 11:48:14 -0500

    update: se añadio el icono en la sidebar y ajustes al tamaño de las tablas en funcion de la pantalla

--- Modulo/static/JS/sidebar.js
+++ Modulo/static/JS/sidebar.js
@@ -2,7 +2,10 @@ function toggleSidebar() {
     document.querySelector("#sidebar .sidebar-header").classList.toggle("collapsive");
     document.querySelector(".sidebar-item-principal").classList.toggle("collapsive");
     document.querySelector("#sidebar").classList.toggle("collapsive");
+    document.querySelector(".toggle-btn").classList.toggle("collapsive");
     document.querySelector("#Inicio").classList.toggle("collapsive");
+    document.querySelector(".img-fluid").classList.toggle("collapsive");
+    document.querySelector(".img-icon").classList.toggle("collapsive");
     document.querySelectorAll("#sidebar ul li a").forEach(link => {link.classList.toggle("collapsive");});
     let toggle = document.querySelector('#sidebar ul li a'); // Selecciona el enlace
 }
@@ -11,11 +14,13 @@ function removeCollapsive() {
     let sidebar = document.querySelector("#sidebar");
     let sidebar_item_principal =document.querySelector(".sidebar-item-principal");
     let Inicio = document.querySelector("#Inicio");
-    let sidebar_header = document.querySelector("#sidebar .sidebar-header");
+    let sidebar_header = document.querySelector(".img-fluid");
     let accordions = document.querySelectorAll("#sidebar ul li a")
+    let icon = document.querySelector(".img-icon")
     if (sidebar.classList.contains("collapsive")) {
         sidebar.classList.remove("collapsive");
         Inicio.classList.remove("collapsive");
+        icon.classList.remove("collapsive");
         sidebar_item_principal.classList.remove("collapsive");
         sidebar_header.classList.remove("collapsive");
         accordions.forEach(link => {link.classList.remove("collapsive");});
