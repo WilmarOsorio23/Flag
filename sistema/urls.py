8a100e98f6d57b4601e543ded1ef7c59e21d7b40
sebastianrestrepo0 <sebastian.restrepo@flagsoluciones.com>
2026-01-21 19:55:14 -0500

    Update urls.py

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,14 +1,14 @@
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+def healthz(_request):
+    return HttpResponse("ok", content_type="text/plain")
+
 urlpatterns = [
+    path("healthz", healthz),
     path("admin/", admin.site.urls),
     path("", include("Modulo.urls")),
-]
-
-# SOLO en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=getattr(settings, "STATIC_ROOT", None))
-    urlpatterns += static(settings.MEDIA_URL, document_root=getattr(settings, "MEDIA_ROOT", None))
+] + static(settings.STATIC_URL, document_root=getattr(settings, "STATIC_ROOT", None)) + static(settings.MEDIA_URL, document_root=getattr(settings, "MEDIA_ROOT", None))
