5c2461a42b2487accc0f708f2c8f46d374f504b0
sebastianrestrepo0 <sebastian.restrepo@flagsoluciones.com>
2026-01-21 20:23:02 -0500

    Update base.py

--- sistema/settings/base.py
+++ sistema/settings/base.py
@@ -60,9 +60,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Modulo", "static"),
-]
+STATICFILES_DIRS = []
 
 MEDIA_URL = "/media/"
 
